211d56ef2b4d87b9178f355e35c7e18a3646c6d1
Jeff Haupt <66330433+jeff1haupt@users.noreply.github.com>
2020-09-02 20:35:39 -0400

    work on dropdown menu
    adding more javascript and Bulma classes for dropdown menu

--- js/app.js
+++ js/app.js
@@ -1,6 +1,12 @@
 const menu = document.querySelector('.navbar-menu');
 const hamburgerMenu = document.querySelector('.navbar-burger');
 
+const dropdown = document.querySelector('.dropdown');
+
 hamburgerMenu.addEventListener('click', function() {
   menu.classList.toggle('is-active');
+});
+
+dropdown.addEventListener('hover', function() {
+  dropdown.classList.toggle('is-active');
 });
